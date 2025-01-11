ff3da47bf70c49d72d0293f7ec023b8a936a9f48
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-01-11 14:32:51 +0700

    update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -45,19 +45,45 @@ export default function About() {
           </div>
         </div>
         <div className="flex gap-20 items-center justify-evenly">
-          <div>
+          <div className="w-[480px] h-[442px] group bg-indigo-50 rounded-3xl p-8 flex flex-col justify-center hover:justify-between items-center">
             <Image
-              src={"./ebrw.svg"}
-              width={480}
-              height={442}
-              alt="info sat ebrw"></Image>
+              src={"./ebrw-main.svg"}
+              width={259}
+              height={259}
+              alt="info sat ebrw"
+              className="transition-all group-hover:w-[162px] group-hover:h-[162px]"></Image>
+            <div className="text-indigo-700 font-semibold text-[44px] group-hover:text-[38px]">
+              EBRW
+            </div>
+            <div className="text-lg text-gray-500 hidden group-hover:block transition-all">
+              <span className="text-indigo-500 font-medium">Reading Test:</span>{" "}
+              Read and understand the passages and answer related questions.{" "}
+              <br /><br />{" "}
+              <span className="text-indigo-500 font-medium">
+                Writing and Language Test:
+              </span>{" "}
+              Edit text to improve grammar, sentence structure, and meaning.
+            </div>
           </div>
-          <div>
+          <div className="w-[480px] h-[442px] group bg-indigo-50 rounded-3xl p-8 flex flex-col justify-center hover:justify-between items-center">
             <Image
-              src={"./math.svg"}
-              width={480}
-              height={442}
-              alt="info sat math"></Image>
+              src={"./math-main.svg"}
+              width={259}
+              height={259}
+              alt="info sat ebrw"
+              className="transition-all group-hover:w-[162px] group-hover:h-[162px]"></Image>
+            <div className="text-indigo-700 font-semibold text-[44px] group-hover:text-[38px]">
+              Math
+            </div>
+            <div className="text-lg text-gray-500 hidden group-hover:block transition-all">
+              <span className="text-indigo-500 font-medium">
+                There are two sections:
+              </span>{" "}
+              one where computers are not allowed and one where computers are
+              allowed.
+              <br /> <br />Content includes algebra, data analysis, and some advanced
+              math concepts.
+            </div>
           </div>
         </div>
         <div className="text-gray-700 text-lg ">
